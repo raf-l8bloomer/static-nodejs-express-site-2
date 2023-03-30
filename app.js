72279cb9ec0568e4ca5772a7a02ab64c96ff9d98
rafael.lumang <lumang.rafael@gmail.com>
2023-03-29 23:04:07 -0700

    Added 404 and global error handlers but does not work with /project/noroute"

--- app.js
+++ app.js
@@ -8,7 +8,14 @@ app.set('view engine', 'pug');
 //static middleware setup
 app.use('/static', express.static('public'));
 
+// 500 error status test
 
+// app.use((req, res, next) => {
+//     const err = new Error('Oh naurrr!');
+//     err.status = 500;
+//     err.message = 'Oh naurrr!';
+//     next(err);
+// })
 
 //routes setup
 app.get('/', (req, res) => {
@@ -25,19 +32,28 @@ app.get('/project/:id', (req, res) => {
 
 })
 
-//404 error handler
+/* ERROR HANDLERS */
+/* 404 handler to catch undefined or non-existent route requests */ 
 
 app.use((req, res, next) => {
     const err = new Error('hey girl, this route does not exist')
     err.status = 404;
+    err.message = 'hey girl, this route does not exist';
     next(err);
 })
 
+/* Global error handler */
 
 app.use((err, req, res, next) => {
+   if (err.status === 404) {
     res.locals.error = err;
-    res.status(err.status);
-    res.render('error', err);
+    res.render('error');
+   } else {
+    err.message = err.message || ' Oopsie! Looks like the server got a poopsie!';
+    res.status = err.status || 500;
+    res.locals.error = err;
+    res.render('error');
+   }
 })
 
 app.listen(3000, () => {
